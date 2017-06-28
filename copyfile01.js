fd62049152aa744236332d383740a3f470e8fb40
Paul Ramnora <pramnora@yahoo.com>
2017-06-28 10:48:15 +0100

    Create copyfile01.js
    Created JScript file: CopyFile command

---
+++ copyfile01.js
@@ -0,0 +1,6 @@
+//*** Code borrowed from...
+//    http://www.vbsedit.com/html/3481a0bc-3fed-46cd-98a0-f3a53adfa0e1.asp
+
+// JScript
+var FSO = WScript.CreateObject("Scripting.FileSystemObject");
+FSO.CopyFile("New Text Document.txt", "New Text Document 2.txt");
